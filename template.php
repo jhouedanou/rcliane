ab8bbd10c21f0c4746bd4956098209dec796c883
Jean Luc Houedanou <jhouedanou@gmail.com>
2013-06-14 17:28:23 +0100

    correction bug template.php

--- template.php
+++ template.php
@@ -2,29 +2,28 @@
 //function mytheme_preprocess_html(&$variables) {
   //  $variables['connex'] = block_get_blocks_by_region('connex');
 //}
-    drupal_add_js(drupal_get_path('theme', 'BlurDrupal') . 'js/jquery-1.7.2.min.js');
-    drupal_add_js(drupal_get_path('theme', 'BlurDrupal') . 'js/cufon-yui.js');
-    drupal_add_js(drupal_get_path('theme', 'BlurDrupal') . 'js/ChunkFive_400.font.js');
-    drupal_add_js(drupal_get_path('theme', 'BlurDrupal') . 'js/cufon-replace.js');
-    drupal_add_js(drupal_get_path('theme', 'BlurDrupal') . 'js/easing.js" type="text/javascript');
-    drupal_add_js(drupal_get_path('theme', 'BlurDrupal') . 'js/jquery.ui.totop.js" type="text/javascript');
-    drupal_add_js(drupal_get_path('theme', 'BlurDrupal') . 'js/jquery.cycle.min.js');
-    drupal_add_js(drupal_get_path('theme', 'BlurDrupal') . 'js/jquery.capty.min.js');
-     drupal_add_js(drupal_get_path('theme', 'BlurDrupal') . 'js/bootstrap-transition.js');
-    drupal_add_js(drupal_get_path('theme', 'BlurDrupal') . 'js/bootstrap-alert.js');
-    drupal_add_js(drupal_get_path('theme', 'BlurDrupal') . 'js/bootstrap-modal.js');
-    drupal_add_js(drupal_get_path('theme', 'BlurDrupal') . 'js/bootstrap-dropdown.js');
-    drupal_add_js(drupal_get_path('theme', 'BlurDrupal') . 'js/bootstrap-scrollspy.js');
-    drupal_add_js(drupal_get_path('theme', 'BlurDrupal') . 'js/bootstrap-tab.js');
-    drupal_add_js(drupal_get_path('theme', 'BlurDrupal') . 'js/bootstrap-tooltip.js');
-    drupal_add_js(drupal_get_path('theme', 'BlurDrupal') . 'js/bootstrap-popover.js');
-    drupal_add_js(drupal_get_path('theme', 'BlurDrupal') . 'js/bootstrap-button.js');
-    drupal_add_js(drupal_get_path('theme', 'BlurDrupal') . 'js/bootstrap-collapse.js');
-    drupal_add_js(drupal_get_path('theme', 'BlurDrupal') . 'js/bootstrap-carousel.js');
-    drupal_add_js(drupal_get_path('theme', 'BlurDrupal') . 'js/bootstrap-typeahead.js');
-    drupal_add_js(drupal_get_path('theme', 'BlurDrupal') . 'js/bootstrap-affix.js');
-    drupal_add_js(drupal_get_path('theme', 'BlurDrupal') . 'js/application.js');
-    drupal_add_js(drupal_get_path('theme', 'BlurDrupal') . 'js/functions.js" type="text/javascript');
+    drupal_add_js(drupal_get_path('theme', 'rcliane') . '/js/jquery-1.7.2.min.js');
+    drupal_add_js(drupal_get_path('theme', 'rcliane') . '/js/cufon-yui.js');
+    drupal_add_js(drupal_get_path('theme', 'rcliane') . '/js/ChunkFive_400.font.js');
+    drupal_add_js(drupal_get_path('theme', 'rcliane') . '/js/cufon-replace.js');
+    drupal_add_js(drupal_get_path('theme', 'rcliane') . '/js/easing.js');
+    drupal_add_js(drupal_get_path('theme', 'rcliane') . '/js/jquery.ui.totop.js');
+    drupal_add_js(drupal_get_path('theme', 'rcliane') . '/js/jquery.cycle.min.js');
+    drupal_add_js(drupal_get_path('theme', 'rcliane') . '/js/jquery.capty.min.js');
+     drupal_add_js(drupal_get_path('theme', 'rcliane') . '/js/bootstrap-transition.js');
+    drupal_add_js(drupal_get_path('theme', 'rcliane') . '/js/bootstrap-alert.js');
+    drupal_add_js(drupal_get_path('theme', 'rcliane') . '/js/bootstrap-modal.js');
+    drupal_add_js(drupal_get_path('theme', 'rcliane') . '/js/bootstrap-dropdown.js');
+    drupal_add_js(drupal_get_path('theme', 'rcliane') . '/js/bootstrap-scrollspy.js');
+    drupal_add_js(drupal_get_path('theme', 'rcliane') . '/js/bootstrap-tab.js');
+    drupal_add_js(drupal_get_path('theme', 'rcliane') . '/js/bootstrap-tooltip.js');
+    drupal_add_js(drupal_get_path('theme', 'rcliane') . '/js/bootstrap-popover.js');
+    drupal_add_js(drupal_get_path('theme', 'rcliane') . '/js/bootstrap-button.js');
+    drupal_add_js(drupal_get_path('theme', 'rcliane') . '/js/bootstrap-collapse.js');
+    drupal_add_js(drupal_get_path('theme', 'rcliane') . '/js/bootstrap-carousel.js');
+    drupal_add_js(drupal_get_path('theme', 'rcliane') . '/js/bootstrap-typeahead.js');
+    drupal_add_js(drupal_get_path('theme', 'rcliane') . '/js/bootstrap-affix.js');
+    drupal_add_js(drupal_get_path('theme', 'rcliane') . '/js/functions.js');
 
 
 
